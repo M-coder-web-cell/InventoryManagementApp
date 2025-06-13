957dcc8aed6dc9b6cb81a87b226a78ada53d07cd
HackRogue <mazumdarh549@gmail.com>
2025-06-13 16:45:43 +0530

    removed the unnecessary code due to previous logic from server

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -5,17 +5,11 @@ import { AI_call, LLMhandling} from "../controllers/Ai_apiControllers.js";
 
 const ProductRouter = express.Router();
 
-//To create New Products
+
 ProductRouter.route('/createProduct').post(protect, createProduct);
-ProductRouter.route('/createProductChat').post(protect, AI_call, createProduct);
-
-// Update
 ProductRouter.route('/updateProduct/:id').put(protect, updateProduct);
-ProductRouter.route('/updateProductChat/:id').put(protect, AI_call, updateProduct);
-
-// Delete
 ProductRouter.route('/deleteProduct/:id').delete(protect, deleteProduct);
-ProductRouter.route('/deleteProductChat/:id').delete(protect, AI_call, deleteProduct);
+
 
 ProductRouter.route('/LLMChat').post(protect, AI_call, LLMhandling);
 // Get all products
